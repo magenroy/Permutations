0b5b2b4e2c70bef742b9d082e103c87afd9b4c3e
Roy Magen <magenroy@gmail.com>
2015-07-18 22:45:48 -0400

    Added primitive IO.

--- Permute.hs
+++ Permute.hs
@@ -1,20 +1,18 @@
 module Main where
 
-import Permutation
-import Cycle
+import Permutation (Permutation, applyPermutation, simplifyPermutation)
 import System.Environment (getArgs)
-import Data.List
-import Data.Set
 
 helptext = "invalid input"
 
 
-parse :: String -> Permutation Int
-parse = toPermutation . fmap (fmap read . words . Data.List.filter (`notElem` "()")) . groupBy (\_ b -> b /= '(')
+parse = read :: String -> Permutation Int
+--parse = toPermutation . fmap (fmap read . words . Data.List.filter (`notElem` "()")) . groupBy (\_ b -> b /= '(')
 
 
 main = getArgs >>= permuteIO
 
+permuteIO :: [String] -> IO ()
 permuteIO ("apply":x:s) = print $ applyPermutation (parse $ unwords s) $ read x
-permuteIO ("simplify":s) = print $ parse $ unwords s
+permuteIO ("simplify":s) = print $ simplifyPermutation $ parse $ unwords s
 permuteIO _ = putStrLn helptext
